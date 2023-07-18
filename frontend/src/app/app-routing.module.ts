ab873a10ce6c2c93c2a8838d303031dae918449a
Elsa Ko <eko095@uottawa.ca>
2023-07-18 13:05:01 +0000

    Fix Navigation & Visuals - refactoring styles - cart/help pages made

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuPageComponent } from './components/menu-page/menu-page.component';
+import { HelpComponent } from './components/help/help.component';
+import { CartComponent } from './components/cart/cart.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/menu', pathMatch: 'full'},
-  {path: 'menu', component: MenuPageComponent}
+  {path: 'menu', component: MenuPageComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'help', component: HelpComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,13 +9,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { NavComponent } from './components/nav/nav.component';
 import { DrinkDetailsComponent } from './components/drink-details/drink-details.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HelpComponent } from './components/help/help.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuPageComponent,
     NavComponent,
-    DrinkDetailsComponent
+    DrinkDetailsComponent,
+    CartComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
